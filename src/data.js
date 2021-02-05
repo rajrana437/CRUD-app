487b4d24999a654ccd3211650675b613486f8305
Raj <rajrana437@gmail.com>
2021-02-05 10:03:43 +0530

    updated app

---
+++ src/data.js
@@ -0,0 +1,14 @@
+const userList = [
+    {
+        id: 1,
+        name: 'Raj',
+        usernme: 'Raj Rana'
+    },
+    {
+        id: 2,
+        name: 'Birgit',
+        username: 'Birgit Boswald'
+    }
+];
+
+export default userList;
